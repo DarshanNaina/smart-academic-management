7d0c2b40202d078bc3cd923b2d950e1e4deadbe9
Darshan Naina <nenadarshan405@gmail.com>
2026-04-20 01:11:56 +0530

    fix python version for psycopg2

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "5000"))
+    port = int(os.getenv("PORT", "10000"))
     app.run(host="0.0.0.0", port=port)
     
 
